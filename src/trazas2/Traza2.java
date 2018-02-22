3d7b83f0dd9a60c370002150d312808f8eee68e7
Víctor J. Fernández <victor.fernandezgil@educa.madrid.org>
2018-02-22 16:50:51 +0100

    3

---
+++ src/trazas2/Traza2.java
@@ -0,0 +1,35 @@
+package trazas2;
+
+import java.util.*;
+public class Traza2 {
+
+    
+    
+    public int bar(String s, int i, Set<Integer> k) {
+        s = "dum";
+        i++;
+        Set<Integer> k2 = k;
+        k2.addAll(Arrays.asList(7, 8));
+        return k2.size();
+    }
+    
+    public void correr() {
+        Set<Integer> miSet = new TreeSet<>();
+        miSet.addAll(Arrays.asList(4, 5, 6, 5, 4, 2, 9, 2 + 4));
+        String s = "foo";
+        int j = 22;
+        // s es inmutable; j pasa por valor; miSet es mutable.
+        bar(s, j, miSet);
+        // nada de lo que haya ocurrido en bar puede alterar s porque es inmutable
+        // nada de lo que haya ocurrido en bar cambia j, porque bar ha recibido sólo el valor de j (copiado)
+        System.out.format("%s %d - ", s, j); // Sin mirar lo que pase en bar: s es "foo" y j es 22.
+        // miSet sí ha podido ser alterado por bar, porque es mutable.
+        for (int i : miSet) {
+            System.out.print(i + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Traza2().correr();
+    }
+}
